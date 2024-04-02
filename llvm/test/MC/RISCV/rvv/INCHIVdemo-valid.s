35af4d4479c9470e61d18e575dcc1f411cd830d5
punkyc <ychen0025@gmail.com>
2024-04-02 09:50:23 +0800

    add asm support for other cm/cr instructions

--- llvm/test/MC/RISCV/rvv/INCHIVdemo-valid.s
+++ llvm/test/MC/RISCV/rvv/INCHIVdemo-valid.s
@@ -32,3 +32,40 @@ inchi.madd32 a0, a1, a2
 # CHECK-ENCODING: [0x0b,0x85,0xc5,0x06]
 # CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
 # CHECK-UNKNOWN: 0b 85 c5 06 <unknown>
+
+inchi.vmov.cmx a0, a1, a2
+# CHECK-INST: inchi.vmov.cmx a0, a1, a2
+# CHECK-ENCODING: [0x0b,0xb5,0xc5,0x06]
+# CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
+# CHECK-UNKNOWN: 0b b5 c5 06 <unknown>
+
+inchi.vmov.cmy a0, a1, a2
+# CHECK-INST: inchi.vmov.cmy a0, a1, a2
+# CHECK-ENCODING: [0x0b,0xc5,0xc5,0x06]
+# CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
+# CHECK-UNKNOWN: 0b c5 c5 06 <unknown>
+
+inchi.vmov.crx a0, a1, a2
+# CHECK-INST: inchi.vmov.crx a0, a1, a2
+# CHECK-ENCODING: [0x0b,0xf5,0xc5,0x02]
+# CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
+# CHECK-UNKNOWN: 0b f5 c5 02 <unknown>
+
+inchi.vmadd32.cm a0, a1, a2, a3
+# CHECK-INST: inchi.vmadd32.cm a0, a1, a2, a3
+# CHECK-ENCODING: [0x0b,0xd5,0xc5,0x6e]
+# CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
+# CHECK-UNKNOWN: 0b d5 c5 6e <unknown>
+
+inchi.vmadd32.cr a0, a1, a2, a3
+# CHECK-INST: inchi.vmadd32.cr a0, a1, a2, a3
+# CHECK-ENCODING: [0x0b,0xf5,0xc5,0x6e]
+# CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
+# CHECK-UNKNOWN: 0b f5 c5 6e <unknown>
+
+inchi.vmadd32.cm.ddr a0, a1, a2, a3
+# CHECK-INST: inchi.vmadd32.cm.ddr a0, a1, a2, a3
+# CHECK-ENCODING: [0x0b,0xe5,0xc5,0x6e]
+# CHECK-ERROR: instruction requires the following: 'xinchivdemo' (INCHI Vector Extensions for Dot){{$}}
+# CHECK-UNKNOWN: 0b e5 c5 6e <unknown>
+
